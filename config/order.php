3b937b5a8f28b93c4f9c9e5e95e93a634e62234d
Rezza Kurniawan <rezza.kurniawan@gmail.com>
2022-04-21 01:52:07 +0700

    remove strong dependency with user model

--- config/order.php
+++ config/order.php
@@ -2,7 +2,5 @@
 
 return [
     'order_model'   =>  \Erkurn\LaravelOrder\Models\Order::class,
-    'order_item_model'  =>  \Erkurn\LaravelOrder\Models\OrderItem::class,
-    'user_model'    =>  \App\Models\User::class,
-    'customer_model'    =>  \App\Models\User::class,
+    'order_item_model'  =>  \Erkurn\LaravelOrder\Models\OrderItem::class
 ];
